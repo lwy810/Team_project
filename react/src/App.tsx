6e820c42e77dee5bb1b1cc78a1c3f7903e72247f
lwy810 <lwy810@naver.con>
2025-07-30 05:44:09 +0900

    Update App.tsx

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Login from './employee/Login'; // login 컴포넌트 임포트
 import Signup from './employee/Signup'; // login 컴포넌트 임포트
